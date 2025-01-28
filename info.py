f69a55d3842b196d13e202e336359aaa83dc9ef5
darknoor0 <noorshaik143143@gmail.com>
2025-01-28 20:20:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7422106744:AAEaxz24bJ8lODmJ-PpcBTQrJTgG8lW
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1729311491').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/darknoor04") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002325566167'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002374033468'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/telegu_movies_04')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002325566167').split()]
 #---------------------------------------------------------------
